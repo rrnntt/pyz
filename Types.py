7224f14dca323c209e7f2013d309c81d95c2aab5
rrnntt <rrnntt@windows>
2013-03-14 13:16:11 +0000

    Binary operations for Double type.

--- Types.py
+++ Types.py
@@ -4,6 +4,9 @@ class Type(object):
     def __init__(self, const):
         self.const = const
         
+    def _combine_const(self, other):
+        return self.const or other.const
+        
 default_const = False
 
 class Double(Type):
@@ -13,7 +16,10 @@ class Double(Type):
     
     def __init__(self, start = -1e300, end = 1e300, const = default_const):
         Type.__init__(self, const)
-        self.domain = sympy.Interval(start,end)
+        if isinstance( start, sympy.Set ):
+            self.domain = start
+        else:
+            self.domain = sympy.Interval(start,end)
             
     def __str__(self):
         out = 'Double(' + str(self.domain)
@@ -21,21 +27,58 @@ class Double(Type):
             out += ' const'
         return  out + ')'
     
-    def _check_overflow(self, start, end):
-        if start <= self.dmin:
-            raise Exception('Double negative overflow')
-        if end >= self.dmax:
-            raise Exception('Double positive overflow')
+    def _check_overflow(self, dom ):
+        if isinstance( dom, sympy.Interval ):
+            if dom.start <= self.dmin:
+                raise Exception('Double negative overflow')
+            if dom.end >= self.dmax:
+                raise Exception('Double positive overflow')
+        else:
+            raise Exception("Domain incompatible with Double: " + str(dom))
+        
+    def _add_intervals(self, ival1, ival2):
+        """Return domain of addition of two doubles lying in intervals ival1 and ival2."""
+        start = ival1.start + ival2.start
+        end = ival1.end + ival2.end
+        ival = sympy.Interval(start, end)
+        self._check_overflow( ival )
+        return ival
+    
+    def _sub_intervals(self, ival1, ival2):
+        """Return domain of subtraction of two doubles lying in intervals ival1 and ival2."""
+        start = ival1.start - ival2.end
+        end = ival1.end - ival2.start
+        ival = sympy.Interval(start, end)
+        self._check_overflow( ival )
+        return ival
+    
+    def _bin_operation(self, other, op):
+        dom1 = self.domain
+        dom2 = other.domain
+        dom = sympy.EmptySet()
+        if isinstance( dom1, sympy.Union ):
+            if isinstance( dom2, sympy.Union ):
+                for d1 in dom1.args:
+                    for d2 in dom2.args:
+                        dom |= op( d1, d2 )
+            else:
+                for d1 in dom1.args:
+                    dom |= op( d1, dom2 )
+        elif isinstance( dom2, sympy.Union ):
+            for d2 in dom2.args:
+                dom |= op( dom1, d2 )
+        else:
+            dom = op(dom1, dom2)
+        return Double( dom )
+    
     
     def __add__(self, other):
-        start = self.domain.start + other.domain.start
-        end = self.domain.end + other.domain.end
-        self._check_overflow(start, end)
-        return Double(start, end)
+        return self._bin_operation(other, self._add_intervals)
     
     def __sub__(self, other):
-        start = self.domain.start - other.domain.end
-        end = self.domain.end - other.domain.start
-        self._check_overflow(start, end)
-        return Double(start, end)
+        return self._bin_operation(other, self._sub_intervals)
+    
+    def __or__(self, other):
+        dom = self.domain | other.domain
+        return Double( dom )
     
--- test_Types.py
+++ test_Types.py
@@ -10,12 +10,43 @@ class TestTypes(unittest.TestCase):
         typ = Double(const = True)
         self.assertTrue( typ.const )
         
+        dt = Double(sympy.Interval(1,2))
+        self.assertTrue(isinstance(dt.domain, sympy.Interval))
+        self.assertEqual(dt.domain.start, 1)
+        self.assertEqual(dt.domain.end, 2)
+
+        dt = Double(sympy.Interval(1,2).union(sympy.Interval(3,4)))
+        dom = dt.domain
+        self.assertTrue(isinstance(dom, sympy.Union))
+        
         x = sympy.Symbol('x')
-        y = eval('2*x+1')
+        y = eval('1+2*x')
         print type(x),y.args,str(x)
         
         d1 = Double(1,2)
         d2 = Double(3,4)
-        d1 += d2
-        print d1
+        d = d1 + d2
+        print d
+        self.assertTrue(isinstance(d.domain, sympy.Interval))
+        self.assertEqual(d.domain.start, 4)
+        self.assertEqual(d.domain.end, 6)
+        self.assertEqual( str(d.domain), '[4, 6]')
+        
+        d1 = Double(1,2)
+        d2 = Double(10,11)
+        d =  d1 | d2
+        dom = d.domain
+        self.assertTrue(isinstance(dom, sympy.Union))
+        self.assertEqual( str(dom), '[1, 2] U [10, 11]')
+        dd = d + d1
+        print dd
+        self.assertEqual( str(dd.domain), '[2, 4] U [11, 13]')
+        dd = d - d1
+        print dd
+        self.assertEqual( str(dd.domain), '[-1, 1] U [8, 10]')
+        d -= d1
+        print d
+        #self.assertEqual( str(dd.domain), '[-1, 1] U [8, 10]')
+        
+
     
