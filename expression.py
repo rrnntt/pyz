afb46a57f61abe70c3c86721c3342b7ae6e31983
rrnntt <rrnntt@windows>
2013-03-15 17:07:24 +0000

    Variable assignment.

--- expression.py
+++ expression.py
@@ -1,4 +1,5 @@
 from namespace import *
+from variable import *
 
 class Expression(object):
     
@@ -12,5 +13,13 @@ class Assignment(Expression):
     
     def __init__(self, namespace, var, expr):
         Expression.__init__(self, namespace)
+        if var.name == None:
+            raise Exception('Assignment to temporary variable (rvalue).')
+        if type(var) != type(expr):
+            raise Exception('Incompatible types in assignment.')
         self.var = var
         self.expr = expr 
+        
+    def run(self):
+        self.var.typ = self.expr.typ
+        
--- test_expression.py
+++ test_expression.py
@@ -4,6 +4,17 @@ from expression import *
 
 class TestExpression(unittest.TestCase):
     
-    def test_expression(self):
-        ns = Namespace()
-        e = Expression()
+    def test_assignment(self):
+        ns = None
+        x = Variable(ns, 'x', Double())
+        y = Variable(ns, 'y', Double(1,2))
+        self.assertEqual( str(x), 'var x: Double([-1.0e+300, 1.0e+300])' )
+        self.assertEqual( str(y), 'var y: Double([1, 2])' )
+        ass = Assignment(ns, x, y)
+        ass.run()
+        self.assertEqual( str(x), 'var x: Double([1, 2])' )
+        
+        Assignment(ns, y, 2*y).run()
+        self.assertEqual( str(x), 'var x: Double([1, 2])' )
+        self.assertEqual( str(y), 'var y: Double([2, 4])' )
+        
